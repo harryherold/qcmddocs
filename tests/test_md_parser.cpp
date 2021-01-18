94481f41020a5e09c76834fae9ecfd64cfb40aef
Christian von Wascinski <harryherold@googlemail.com>
2021-01-18 17:08:07 +0100

    Implement md header parser in test.

--- tests/test_md_parser.cpp
+++ tests/test_md_parser.cpp
@@ -1,68 +1,110 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
 
 #include <algorithm>
+#include <functional>
 
 #include <QString>
 
 #include <catch.hpp>
 
-int
-getIndent(const QString &s)
-{
-    int spaces = 0;
-    for(const auto & c: s)
-    {
-        if(c != QLatin1Char(' '))
-        {
-            break;
-        }
-        spaces++;
-    }
-    return spaces;
-}
-
-QString
+inline QString
 indentString(int indent, const QString &s)
 {
     QString prefix(indent, QLatin1Char(' '));
     return prefix + s;
 }
 
-int
-getHeadingLevel(const QString & s)
+inline int
+count_char(const QString &s, QLatin1Char character, int offset = 0)
 {
-    constexpr auto hashTag = QLatin1Char('#');
-    int heading = 0;
-    int indent = getIndent(s);
-    if(s.at(indent) == hashTag)
+    int count = 0;
+    for (int i = offset; i < s.count(); i++)
     {
-        int strPos = indent;
-
-        // TODO use find_if
-        for(;strPos < s.count() && s[strPos] == hashTag; strPos++){}
-
-        if(strPos < s.count() && s[strPos] == QLatin1Char(' '))
+        if (s[i] == character)
         {
-            heading = strPos - indent;
+            count++;
+        }
+        else
+        {
+            break;
         }
     }
-    return heading;
+    return count;
 }
 
-TEST_CASE( "Determines the indent of a QString.", "[getIndent]" ) {
+inline bool
+contains_only(const QString &s, QLatin1Char character)
+{
+    return count_char(s, character) == s.count();
+}
+
+using nextFunctor = std::function<QString(void)>;
+
+int
+getHeadingLevel(const QString &s, nextFunctor nextCb)
+{
+    constexpr auto dash          = QLatin1Char('-');
+    constexpr auto equals        = QLatin1Char('=');
+    constexpr auto hashTag       = QLatin1Char('#');
+    constexpr auto whiteSpace    = QLatin1Char(' ');
+    auto           trimmedString = s.trimmed();
+
+    int indent = count_char(trimmedString, whiteSpace);
+
+    if (trimmedString.at(indent) == hashTag)
+    {
+        int heading = count_char(trimmedString, hashTag, indent);
+        int strPos  = indent + heading;
+
+        if (strPos < trimmedString.count() && trimmedString[strPos] == whiteSpace)
+        {
+            return heading;
+        }
+    }
+    auto nextLine = nextCb().trimmed();
+    if (nextLine.isEmpty())
+        return 0;
+
+    if (contains_only(nextLine, equals) && trimmedString.count() > 0)
+    {
+        return 1;
+    }
+    else if (contains_only(nextLine, dash) && trimmedString.count() > 0)
+    {
+        return 2;
+    }
+    return 0;
+}
+
+TEST_CASE("Determines the indent of a QString.", "[getIndent]")
+{
     QString s1{"Hallo"};
+    auto    getIndent = [](const QString &s) -> int { return count_char(s, QLatin1Char(' ')); };
     REQUIRE(getIndent(indentString(5, s1)) == 5);
     REQUIRE(getIndent(indentString(1, s1)) == 1);
     REQUIRE(getIndent(s1) == 0);
     REQUIRE(getIndent(QString("")) == 0);
 }
 
-TEST_CASE( "Determines the heading level of a markdown text.", "[getHeadingLevel]" ) {
-    REQUIRE(getHeadingLevel("# Foo") == 1);
-    REQUIRE(getHeadingLevel("## Foo") == 2);
-    REQUIRE(getHeadingLevel("### Foo") == 3);
-    REQUIRE(getHeadingLevel("#### Foo") == 4);
-    REQUIRE(getHeadingLevel("####Foo") == 0);
-    REQUIRE(getHeadingLevel("###") == 0);
-    REQUIRE(getHeadingLevel("  #### Foo") == 4);
+TEST_CASE("Determines the heading level of a markdown text.", "[getHeadingLevel]")
+{
+    auto emptyString = []() -> QString { return QString{""}; };
+
+    REQUIRE(getHeadingLevel("# Foo", emptyString) == 1);
+    REQUIRE(getHeadingLevel("## Foo", emptyString) == 2);
+    REQUIRE(getHeadingLevel("### Foo", emptyString) == 3);
+    REQUIRE(getHeadingLevel("#### Foo", emptyString) == 4);
+    REQUIRE(getHeadingLevel("####Foo", emptyString) == 0);
+    REQUIRE(getHeadingLevel("###", emptyString) == 0);
+    REQUIRE(getHeadingLevel("  #### Foo", emptyString) == 4);
+    REQUIRE(getHeadingLevel("# ", emptyString) == 0);
+
+    auto f1 = []() -> QString { return QString{"-------"}; };
+    REQUIRE(getHeadingLevel("Foo", f1) == 2);
+
+    auto f2 = []() -> QString { return QString{"======"}; };
+    REQUIRE(getHeadingLevel("Foo", f2) == 1);
+
+    auto f3 = []() -> QString { return QString{"==Hallo"}; };
+    REQUIRE(getHeadingLevel("Foo", f3) == 0);
 }
